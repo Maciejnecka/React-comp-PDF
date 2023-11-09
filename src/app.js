6f374130e6edfa434c486fc8c38507d7b39e567b
Maciej Necka <macieknecka@gmail.com>
2023-11-09 08:48:30 +0100

    21/77 02. Stan

--- src/app.js
+++ src/app.js
@@ -5,8 +5,36 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
 
-const App = () => {
-  return <h1>Hello</h1>;
-};
+// Montowanie oraz aktualizacja
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello World</h1>;
+//   }
+// }
+
+// Tylko raz przy montowaniu
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
+//   render() {
+//     return <h1>{this.state.counter}</h1>;
+//   }
+// }
+
+// Uruchamianie na etapie montowani
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+  componentDidMount() {
+    this.id = setInterval(() => console.log('co sekunde...'), 1000);
+  }
+  // Gdy komponent zostanie zaktualizowany
+  componentDidUpdate() {
+    clearInterval(this.id);
+  }
+}
 
 root.render(<App />);
